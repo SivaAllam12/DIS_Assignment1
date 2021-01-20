4f9915bdf3e3c41699e992fce24d41738afa3d2c
Sivaramakrishna Allam <allams@usf.edu>
2021-01-20 15:32:08 -0500

    added ques1

--- Program.cs
+++ Program.cs
@@ -9,8 +9,27 @@ namespace Assignment1
         {
             var p1 = new Program();
             //Console.WriteLine(p1.ques3());
-            Console.WriteLine(p1.ques5()); ;
+            //Console.WriteLine(p1.ques5());
+            ques1(5);
         }
+       private static void ques1(int n)
+        {
+            int i = 0, j = 0, l = 1,k=0;
+            for(i=1;i<=n;i++)
+            {
+                for(j=n;j>=i;j--)
+                {
+                    Console.Write(" ");
+                }
+                for(k=1;k<=l;k++)
+                {
+                    Console.Write('*');
+                }
+                Console.WriteLine();
+                l = l + 2;
+            }
+        }
+                    
         public void ques2()
             {
                 Console.WriteLine("How many pell numbers you want to print");
@@ -28,6 +47,7 @@ namespace Assignment1
                         Console.Write(',');
                 }
             }
+
         public bool ques3()
         {
             int i, j;
